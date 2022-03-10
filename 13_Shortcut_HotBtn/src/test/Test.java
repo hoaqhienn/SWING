e5ffc3039ccb7eea9a813e53b12751c8d1455f26
Davi <hoanghienle69@gmail.com>
2022-03-10 23:30:14 +0700

    update

---
+++ 13_Shortcut_HotBtn/src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+import javax.swing.UIManager;
+
+import view.MenuExampleView;
+
+public class Test {
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			new MenuExampleView();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}	
+	}
+}
